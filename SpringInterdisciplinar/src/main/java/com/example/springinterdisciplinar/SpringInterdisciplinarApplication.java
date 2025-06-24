eabce7132d54d51c48e95070dcabfe1f89bb8acf
LeonardoNakashima <157625776+LeonardoNakashima@users.noreply.github.com>
2025-06-24 15:13:43 -0300

    Adicionando o projeto da API

---
+++ SpringInterdisciplinar/src/main/java/com/example/springinterdisciplinar/SpringInterdisciplinarApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinterdisciplinar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInterdisciplinarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInterdisciplinarApplication.class, args);
+    }
+
+}
---
+++ SpringInterdisciplinar/src/test/java/com/example/springinterdisciplinar/SpringInterdisciplinarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springinterdisciplinar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInterdisciplinarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
