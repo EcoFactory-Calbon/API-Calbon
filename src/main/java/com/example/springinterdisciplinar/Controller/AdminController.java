6072e399ad67ffc1508c46e629c9d2a2d712c213
LeonardoNakashima <157625776+LeonardoNakashima@users.noreply.github.com>
2025-08-25 08:40:22 -0300

    update .gitignore

--- src/main/java/com/example/springinterdisciplinar/Controller/AdminController.java
+++ src/main/java/com/example/springinterdisciplinar/Controller/AdminController.java
@@ -1,8 +1,11 @@
 package com.example.springinterdisciplinar.Controller;
 
+import com.example.springinterdisciplinar.Dto.AdminRequestDTO;
+import com.example.springinterdisciplinar.Dto.AdminResponseDTO;
 import com.example.springinterdisciplinar.Model.Admin;
 import com.example.springinterdisciplinar.Model.Funcionario;
 import com.example.springinterdisciplinar.Service.AdminService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,16 +22,19 @@ public class AdminController {
     }
 
     @GetMapping("/listar")
-    public ResponseEntity<List<Admin>> listarAdmin() {
-        List<Admin> admins = adminService.listar();
+    public ResponseEntity<List<AdminResponseDTO>> listarAdmin() {
+        List<AdminResponseDTO> admins = adminService.listar()
+                .stream()
+                .map(a -> new AdminResponseDTO(a.getEmail(), a.getNome()))
+                .toList();
         return ResponseEntity.ok(admins);
-
     }
 
     @PostMapping("/adicionar")
-    public ResponseEntity<Admin> adicionarFuncionario(@RequestBody Admin admin) {
-        Admin novo = adminService.inserirAdmin(admin);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novo);
+    public ResponseEntity<AdminResponseDTO> adicionarAdmin(@RequestBody @Valid AdminRequestDTO dto) {
+        Admin novo = adminService.inserirAdmin(new Admin(dto.getEmail(), dto.getNome(), dto.getSenha()));
+        AdminResponseDTO response = new AdminResponseDTO(novo.getEmail(), novo.getNome());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @DeleteMapping("/excluir/{email}")
